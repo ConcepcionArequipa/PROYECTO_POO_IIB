6efd8bc54f411758b40d4065521fa8dc0291a535
ConcepcionArequipa <concepcion.arequipa@epn.edu.ec>
2026-01-02 01:32:46 -0500

    Se implemento try-with-resources para cerrar los recursos correctamente

--- src/dao/TramiteDao.java
+++ src/dao/TramiteDao.java
@@ -12,11 +12,11 @@ public class TramiteDao {
 
         String sql = "insert into tramite (solicitante_id, estado, fecha_creacion) " +
                       "values (?, 'pendiente', curdate())";
-        try{
-            Connection con = new Conexion().getConexion();
-            PreparedStatement ps = con.prepareStatement(sql);
+        try(
+                Connection con = new Conexion().getConexion();
+                PreparedStatement ps = con.prepareStatement(sql)
+        ) {
             ps.setInt(1,solicitanteId);
-
             return ps.executeUpdate() > 0;
 
         } catch (Exception e) {
@@ -28,9 +28,10 @@ public class TramiteDao {
     public boolean actualizarEstado(int tramiteId , String estado){
         String sql = "update tramite set estado = ? where id =?";
 
-        try{
-            Connection con = new Conexion().getConexion();
-            PreparedStatement ps = con.prepareStatement(sql);
+        try(
+                Connection con = new Conexion().getConexion();
+                PreparedStatement ps = con.prepareStatement(sql)
+        ) {
 
             ps.setString(1,estado);
             ps.setInt(2,tramiteId);
