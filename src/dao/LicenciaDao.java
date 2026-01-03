e56b4cc673ea67440d7d79d03c461369318d79d7
ConcepcionArequipa <concepcion.arequipa@epn.edu.ec>
2026-01-02 23:23:37 -0500

    Se agrego un metodo para verificar si ya existe una licencia a un tramite

--- src/dao/LicenciaDao.java
+++ src/dao/LicenciaDao.java
@@ -11,9 +11,10 @@ public class LicenciaDao {
                 (tramite_id,numero_licencia,fecha_emision,fecha_vencimiento)
                 values(?,?,curdate(),?)
                 """;
-        try{
-            Connection com = new Conexion().getConexion();
-            PreparedStatement ps = com.prepareStatement(sql);
+        try(
+                Connection con = new Conexion().getConexion();
+                PreparedStatement ps = con.prepareStatement(sql)
+        ) {
 
             ps.setInt(1,l.getTramiteId());
             ps.setString(2, l.getNumeroLicencia());
@@ -22,8 +23,36 @@ public class LicenciaDao {
             return ps.executeUpdate() > 0;
 
         }catch (Exception e){
-            System.out.printf("Error "+e.getMessage());
-            return false;
+            throw new RuntimeException("Error al emitir licencia",e);
+        }
+    }
+
+    //Metodo para verificar si ya existe una Licencia por tramite
+    //Recibe la id del tramite y retorna true si ya existe
+
+    public boolean existeLicencia(int tramiteId){
+
+        //Cuenta cuantas licencias estan asociadas al tramite
+        String sql= "select count(*) from licencia where tramite_id=?";
+        try(
+                Connection con = new Conexion().getConexion();
+                PreparedStatement ps = con.prepareStatement(sql)
+        ){
+            ps.setInt(1,tramiteId);
+
+            //Ejecutar consulta
+
+            ResultSet rs= ps.executeQuery(); //Devuelve el resultado de la consulta, como una fila por count
+
+            //Lee el rsultado
+            if (rs.next()){
+                //Si es mayor que 0 es true, existe licencia
+                return rs.getInt(1)> 0;
+            }
+            return false; //No exite licencia
+        }catch (Exception e){
+            //Excepcion no verificada para errores tecnicos
+            throw new RuntimeException("Error al verificar licencia",e);
         }
     }
 }
